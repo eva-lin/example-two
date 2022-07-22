92cd7a62991701e4aecc0d4f2e7e26e5d2d445cc
eva-lin <tagirmirzoeva@yandex.ru>
2022-07-23 00:32:55 +0300

    задача 1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static  void main (String[] args){
+        //Задача 1
+        System.out.println("Task 1");
+        byte b = 20;
+        short s = 530;
+        int i = 1000;
+        long l = 1000000000L;
+
+        float f =3.14f;
+        double d = 5.7;
+
+        char c  = 'e';
+
+        boolean bl = true;
+
+    }
+}
