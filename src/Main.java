9e79bc94c22c6113c46ff6cb13dc1b02c233f38a
eva-lin <tagirmirzoeva@yandex.ru>
2022-07-25 02:26:44 +0300

    задача 3

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,16 @@ public class Main {
         float weight2 = 82.7f;
         float totalWeight = weight1 + weight2;
         float difWeight = Math.abs(weight1 -weight2);
-        System.out.println("Общий вес: " + totalWeight+ "кг");
-        System.out.println("Разница между весами бойцов: " + difWeight+ "кг");
+        System.out.println("Общий вес: " + totalWeight+ " кг");
+        System.out.println("Разница между весами бойцов: " + difWeight+ " кг");
+
+        //Задача 3
+        System.out.println("Task 3");
+        short bananas = 5;
+        short milk = 200;
+        short iceCream = 2;
+        short eggs = 4;
+        double breakfast = bananas*80 + milk*1.05 + iceCream*100 +eggs*70;
+        System.out.println("Общий вес завтрака: " + breakfast/1000 + " кг");
     }
 }
